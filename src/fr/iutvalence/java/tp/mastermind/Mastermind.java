dae06828ba4d8db98d5cdc739f7de06f208726b1
sebastienjean <baz.jean@gmail.com>
2014-05-02 14:43:16 +0200

    Création d'une classe permettant de représenter le résultat de la comparaison de 2 motifs.
    - le motif à deviner est maintenant un attribut (non statique) dans
    MasterMind
    - le motif proposé est maintenant une variable locale à la méthode jouer
    dans MasterMind

--- src/fr/iutvalence/java/tp/mastermind/Mastermind.java
+++ src/fr/iutvalence/java/tp/mastermind/Mastermind.java
@@ -1,70 +1,54 @@
 package fr.iutvalence.java.tp.mastermind;
 
-// TODO (fait)écrire un commentaire plus précis
 /**
- * Debut de la partie
- * La partie de Mastermind est prete a etre lancer On dispose de plusieurs
- * pions de 8 couleurs differentes Un motif de 4 pions a trouver est genere
- * aleatoirement Vous avez 12 essais pour trouver le motif.
- *  Les jetons rouges annoncent un pion de bonne couleur bien placé 
- *  Les jetons blancs annoncent un pion de bonne couleur mais mal place
- *   On ne met rien quand un pion ne fait pas partie de la combinaison 
- *   La partie se finit quand on a trouve le bon motif ou quand on n'a plus d'essais
+ * Représente une partie de MasterMind
+ * 
  * @author Nicolas et Benjamin
  */
 public class Mastermind
 {
-	// TODO (fait) déplacer le commentaire au bon endroit
 
+	/**
+	 * Nombre d'essais maximum
+	 */
+	public final static int NOMBRE_D_ESSAIS_MAXIMUM = 12;
 
-	// TODO (fait)corriger la déclaration (ce n'est pas une constante)
-	// TODO (fait)renommer
 	/**
-	 * Nombre d'essai maximum
+	 * Le motif à deviner
 	 */
-	public final static int ESSAI_DEFAULT = 12;
-	
-	// TODO (fait) écrire un commentaire 
+	private Motif motifADeviner;
+
 	/**
-	 * Creation d'un motif aleatoire a trouver de 4 pions de couleurs différentes
-	 * 
-	 */
-	public static Motif motifADeviner;
-	
-	/**
-	 * Le motif proposer par l'utilisateur
-	 */
-	public Motif motifPropose;
-	/**
-	 * On cree un tableau pour mettre les pions d'un motif
+	 * Création d'une nouvelle partie de Mastermind (le motif à deviner est
+	 * choisi aléatoirement)
 	 */
 	public Mastermind()
 	{
-		motifADeviner = new Motif();		
+		this.motifADeviner = new Motif();
 	}
 
-	// TODO (fait) écrire un commentaire 
 	/**
-	 * Permet au joueur de proposer un motif à 4 pions 
-	 * On le compare avec le motif a trouver
+	 * Jouer la partie
 	 */
 	public void jouer()
 	{
 		System.out.print("Le motif à trouver:");
-		System.out.println(motifADeviner.toString());
-		int essai = 0;
-		while(essai<ESSAI_DEFAULT)
-		{	
+		System.out.println(this.motifADeviner);
+
+		for (int essai = 1; essai <= NOMBRE_D_ESSAIS_MAXIMUM; essai++)
+		{
 			Motif motifPropose = new Motif();
-			System.out.println(motifPropose.toString());
-			System.out.println(Motif.comparerMotif(motifPropose));
-			essai++;//entre nous c'est complètement faux
-			if(motifPropose == motifADeviner)//enlever l'attribut 
-				{
-					System.out.println( "Vous avez gagner en "+essai+" essai(s)");
-					break;
-				}
+			System.out.println(motifPropose);
+			ResultatComparaisonMotifs resultatComparaisonMotifs = this.motifADeviner.comparerMotif(motifPropose); 
+			System.out.println(resultatComparaisonMotifs);
+			
+			if (resultatComparaisonMotifs.obtenirNombreDePionsBienPlaces() == Motif.NOMBRE_DE_PIONS)
+			{
+				System.out
+						.println("Gagne en " + essai + " essai(s)");
+				return;
+			}
 		}
-		//System.out.println("Rentre ton motif de pions:");
-	}	
+		System.out.println("Perdu");
+	}
 }
--- src/fr/iutvalence/java/tp/mastermind/Motif.java
+++ src/fr/iutvalence/java/tp/mastermind/Motif.java
@@ -1,10 +1,10 @@
 package fr.iutvalence.java.tp.mastermind;
 
+import java.util.Arrays;
 import java.util.Random;
 
-// TODO (fait)écrire un commentaire  plus précis
 /**
- * Un motif est compose de 4 pions parmis 8 couleurs differentes
+ * Représente un motif de matermind, composé de 4 pions parmi un ensemble de couleurs differentes
  * 
  * @author Nicolas et Benjamin
  */
@@ -12,15 +12,15 @@ public class Motif
 {
 
 	/**
-	 * Nombre de pion par essai
+	 * Nombre de pions
 	 */
 	public final static int NOMBRE_DE_PIONS = 4;
+	
 	/**
-	 * Le motif a chercher
+	 * Les pions
 	 */
 	private int[] pions;
 
-	// TODO(fait) écrire un commentaire plus précis
 	/**
 	 * Creer un motif aléatoire
 	 */
@@ -29,90 +29,67 @@ public class Motif
 		this.pions = new int[NOMBRE_DE_PIONS];
 		Random r = new Random();
 		for (int emplacement = 0; emplacement < NOMBRE_DE_PIONS; emplacement++)
-			this.pions[emplacement] = r.nextInt(8);
+			this.pions[emplacement] = r.nextInt(Couleur.values().length);
 	}
 
 	/**
 	 * Fonction qui va comparer le motif a trouver et le motif proposer par
 	 * l'utilisateur
 	 *
-	 * @param motifPropose
+	 * @param autreMotif
 	 *            Le motif a trouver par le joueur
 	 * @return une chaine qui affiche les pions bien placé et mal placé par
 	 *         rapport au motif à trouver
 	 */
-	public static String comparerMotif(Motif motifPropose)
+	public ResultatComparaisonMotifs comparerMotif(Motif autreMotif)
 	{
 		int nombreDePionsBienPlaces = 0;
 		int nombreDePionsMalPlaces = 0;
 
-		boolean[] masqueDuMotifATrouver = new boolean[NOMBRE_DE_PIONS];
-		boolean[] masqueDuMotifPropose = new boolean[NOMBRE_DE_PIONS];
+		boolean[] pionsEncoreATraiterDansLeMotif = new boolean[NOMBRE_DE_PIONS];
+		boolean[] pionsEncoreATraiterDansLAutreMotif = new boolean[NOMBRE_DE_PIONS];
 		
 		for (int numeroDuPion = 0; numeroDuPion < NOMBRE_DE_PIONS; numeroDuPion++)
 		{
-			masqueDuMotifATrouver[numeroDuPion] = true;
-			masqueDuMotifPropose[numeroDuPion] = true;
+			pionsEncoreATraiterDansLeMotif[numeroDuPion] = true;
+			pionsEncoreATraiterDansLAutreMotif[numeroDuPion] = true;
 		}
 		
 		for (int numeroDuPion = 0; numeroDuPion < NOMBRE_DE_PIONS; numeroDuPion++)
 		{
-			if (Mastermind.motifADeviner.getPion(numeroDuPion) == motifPropose.getPion(numeroDuPion))
+			if (this.pions[numeroDuPion] == autreMotif.pions[numeroDuPion])
 			{
 				nombreDePionsBienPlaces++;
-				masqueDuMotifPropose[numeroDuPion] = false;
-				masqueDuMotifATrouver[numeroDuPion] = false;
+				pionsEncoreATraiterDansLAutreMotif[numeroDuPion] = false;
+				pionsEncoreATraiterDansLeMotif[numeroDuPion] = false;
 			}
 			
 		}
 		
 		for (int numeroDuPionDansLeMotifATrouver = 0; numeroDuPionDansLeMotifATrouver < NOMBRE_DE_PIONS; numeroDuPionDansLeMotifATrouver++)
 		{
-			if (!masqueDuMotifATrouver[numeroDuPionDansLeMotifATrouver]) continue;
+			if (!pionsEncoreATraiterDansLeMotif[numeroDuPionDansLeMotifATrouver]) continue;
 			
 			for (int numeroDuPionDansLeMotifPropose = 0; numeroDuPionDansLeMotifPropose < NOMBRE_DE_PIONS; numeroDuPionDansLeMotifPropose++)
 			{
-				if (!masqueDuMotifPropose[numeroDuPionDansLeMotifPropose]) continue;
+				if (!pionsEncoreATraiterDansLAutreMotif[numeroDuPionDansLeMotifPropose]) continue;
 						
-				if (Mastermind.motifADeviner.getPion(numeroDuPionDansLeMotifATrouver) == motifPropose.getPion(numeroDuPionDansLeMotifPropose))
+				if (this.pions[numeroDuPionDansLeMotifATrouver] == autreMotif.pions[numeroDuPionDansLeMotifPropose])
 					{
 						nombreDePionsMalPlaces++;					
-						masqueDuMotifPropose[numeroDuPionDansLeMotifPropose] = false;
-						masqueDuMotifATrouver[numeroDuPionDansLeMotifATrouver] = false;
+						pionsEncoreATraiterDansLAutreMotif[numeroDuPionDansLeMotifPropose] = false;
+						pionsEncoreATraiterDansLeMotif[numeroDuPionDansLeMotifATrouver] = false;
 						break;
 					}
 			}
 		}
-		return "Il y a " + nombreDePionsBienPlaces + " pion(s) bien placé et " + nombreDePionsMalPlaces
-				+ " pion(s) mal placé";
+		return new ResultatComparaisonMotifs(nombreDePionsBienPlaces, nombreDePionsMalPlaces);
 	}
-	/**
-	 * Méthode qui renvoie le caractère à une position donné d'un motif
-	 * @param position
-	 * 		l'indice à l'endroit où l'on cherche l'entier
-	 * @return l'entier à la position donné du motif
-	 */
-	public int getPion(int position)
-	{
-		return this.pions[position];
-	}
-	/**
-	*
-	*/
+
+	@Override
 	public String toString()
-	{ 
-		if(this.pions != null)
-		{
-		String resultat = "";
-		for(int pion : this.pions)
-			resultat += pion; 
-		return resultat;
-		}
-		return "";
+	{
+		return "Motif [pions=" + Arrays.toString(this.pions) + "]";
 	}
-	/**
-	 * méthode qui lit le motif a deviner
-	 * @return un tableau d'entier
-	 */
 
 }
