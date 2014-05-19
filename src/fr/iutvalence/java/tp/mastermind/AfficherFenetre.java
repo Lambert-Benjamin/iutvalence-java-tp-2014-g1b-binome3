1b0e1f16360cf32e99ffa014cd002ea9062edd55
rougen <rougen@PC-B101-04.iut-valence.fr>
2014-05-19 11:31:39 +0200

    définition de menu fenetre

--- src/fr/iutvalence/java/tp/mastermind/AfficherFenetre.java
+++ src/fr/iutvalence/java/tp/mastermind/AfficherFenetre.java
@@ -1,19 +1,31 @@
 package fr.iutvalence.java.tp.mastermind;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JSplitPane;
+import javax.swing.WindowConstants;
 
 /**
  * Affiche la fenetre du mastermind 
  * @author rougen
  *
  */
-public class AfficherFenetre implements Runnable , Affichage
+public class AfficherFenetre implements Runnable , Affichage , ActionListener
 {
 	/**
 	 * fenetre principale du mastermind
 	 */
 	private JFrame fenetre;
-	
+	/**
+	 * L'onglet A Propos du menu
+	 */
+	private JMenuItem menuItemAPropos;
 	
 	@Override
 	public void run()
@@ -21,9 +33,37 @@ public class AfficherFenetre implements Runnable , Affichage
 		this.fenetre = new JFrame();
 		this.fenetre.setSize(300,200);
 		this.fenetre.setTitle("Test de Fenetre");
+
+		this.fenetre.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		
+		JSplitPane panelSimple = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		this.fenetre.add(panelSimple);
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu menu = new JMenu("Menu");
+		JMenuItem menuItemNewPartie = new JMenuItem("Nouvelle Partie");
+		JMenuItem menuItemAbandonner = new JMenuItem("Abandonner(comme un lâche)");
+		this.menuItemAPropos = new JMenuItem("A Propos");
+		menu.add(menuItemNewPartie);
+		menu.add(menuItemAbandonner);
+		menu.add(this.menuItemAPropos);
+		menuItemNewPartie.addActionListener(this);
+		menuItemAbandonner.addActionListener(this);
+		menuBar.add(menu);
+		
+		JMenu niveau = new JMenu("Niveau");
+		JMenuItem menuItem1 = new JMenuItem("Niveau 1");
+		JMenuItem menuItem2 = new JMenuItem("Niveau 2");
+		niveau.add(menuItem1);
+		niveau.add(menuItem2);
+		menuItem1.addActionListener(this);
+		menuItem2.addActionListener(this);
+		menuBar.add(niveau);
+	
+		
+		this.fenetre.setJMenuBar(menuBar);
 		this.fenetre.setVisible(true);
-		
-		
 	}
 
 	@Override
@@ -50,6 +90,22 @@ public class AfficherFenetre implements Runnable , Affichage
 	@Override
 	public void afficherDefaite(Motif motifADeviner)
 	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event)
+	{
+		JMenuItem itemChoisi = (JMenuItem) event.getSource();
+		
+		if (itemChoisi == this.menuItemAPropos)
+		{
+				JOptionPane.showMessageDialog(this.fenetre, "Jeu de mastermind creer par Sir Söpalain et Lord Klëënex","A Propos",JOptionPane.INFORMATION_MESSAGE);
+				return;
+		}
+
+
 		// TODO Auto-generated method stub
 		
 	}
